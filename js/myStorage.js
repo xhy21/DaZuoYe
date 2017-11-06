57c6aa97ce3a23b923846afe44912ae64c4cfef2
xhy21 <211220x@sina.cn>
2017-11-06 09:23:51 +0800

    确定页面的基础结构并用localStorage封装数据

---
+++ js/myStorage.js
@@ -0,0 +1,20 @@
+;(function(){
+  window.ms={
+    set: set,
+    get: get,
+  };
+  function set(key,val){
+
+    localStorage.setItem(key, JSON.stringify(val));
+
+  }
+
+  function get(key){
+    var json=localStorage.getItem(key);
+    if(json){
+      return JSON.parse(json);
+    }
+  }
+})();
+
+var task =ms.get('task');
