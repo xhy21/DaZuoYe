1b332022421c5abdfcca2cb0b7212c3ce4a6b5a5
xhy21 <211220x@sina.cn>
2017-11-06 11:17:36 +0800

    增加添加功能

---
+++ js/main.js
@@ -0,0 +1,25 @@
+;(function(){
+  'use strict';
+new Vue({
+el:'#main',
+  data:{
+  list:[],
+  current:{}
+
+},
+methods:{
+  add:function(){
+var title = this.current.title;
+    if (!title&&title!==0)  alert("输入的内容不能为空！");
+    var todo = Object.assign({},this.current)
+this.list.push(todo);
+  },
+  update:function(){},
+  remove:function(){
+
+  }
+}
+
+});
+
+})();
