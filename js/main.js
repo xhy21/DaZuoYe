19a38fcabd43b928594cabd84079eb8766c8a7ce
xhy21 <211220x@sina.cn>
2017-11-07 10:07:26 +0800

    完成修改模块

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 ;(function(){
   'use strict';
+
+  function copy(obj){
+    return Object.assign({},obj);
+  }
 new Vue({
 el:'#main',
   data:{
@@ -8,15 +12,39 @@ el:'#main',
 
 },
 methods:{
-  add:function(){
-var title = this.current.title;
-    if (!title&&title!==0)  alert("输入的内容不能为空！");
-    var todo = Object.assign({},this.current)
-this.list.push(todo);
+  merge:function(){
+    var is_update,id;
+    var is_update=id=this.current.id;
+    if(is_update){
+      var index =this.list.findIndex(function(item){
+        return item.id == is_update;
+      });
+
+      Vue.set(this.list,index,copy(this.current));
+
+    }else{
+      var title = this.current.title;
+          if (!title&&title!==0)  alert("输入的内容不能为空！");
+          var todo = copy(this.current);
+          todo.id=this.next_id();
+      this.list.push(todo);
+    }
+this.reset_current();
+
   },
-  update:function(){},
+
   remove:function(id){
 this.list.splice(id,1);
+  },
+  next_id:function(){
+    return this.list.length+1;
+  },
+
+  set_current:function(todo){
+  this.current = copy(todo);
+},
+  reset_current:function(){
+    this.set_current({});
   }
 }
 
